bb7c3f7a51d9dcb7f5eb913eb568aea6ecaf0738
Matt Fehskens <matt.fehskens@gmail.com>
2018-12-08 09:08:10 -0500

    Remove double reading of file

--- advent-01/src/main.rs
+++ advent-01/src/main.rs
@@ -4,37 +4,45 @@ use std::fs;
 fn main() {
     let contents = fs::read_to_string("input/input.txt")
         .expect("Something went wrong reading the file");
+    let numbers = read_numbers(&contents);
 
     // problem #1
-    println!("Frequency: {}", get_frequency(&contents));
+    println!("Frequency: {}", get_frequency(&numbers));
     // problem #2
-    println!("First repeat: {}", get_repeat(&contents));
+    println!("First repeat: {}", get_repeat(&numbers));
 }
 
-fn get_frequency(contents: &str) -> i32 {
-    let mut frequency: i32 = 0;
+fn read_numbers(contents: &str) -> Vec<i32> {
+    let mut numbers: Vec<i32> = Vec::new();
 
     for line in contents.lines() {
         let value: i32 = line.parse()
             .expect("Couldn't parse line to number");
 
+        numbers.push(value);
+    }
+
+    numbers
+}
+
+fn get_frequency(numbers: &Vec<i32>) -> i32 {
+    let mut frequency: i32 = 0;
+
+    for value in numbers {
         frequency += value;
     }
 
     frequency
 }
 
-fn get_repeat(contents: &str) -> i32 {
+fn get_repeat(numbers: &Vec<i32>) -> i32 {
     let mut seen = HashSet::new();
     let mut frequency: i32 = 0;
 
     seen.insert(0);
 
     loop {
-        for line in contents.lines() {
-            let value: i32 = line.parse()
-                .expect("Couldn't parse line to number");
-
+        for value in numbers {
             frequency += value;
 
             if seen.contains(&frequency) {
